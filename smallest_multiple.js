71f284187839a3f9f00649e1a7c957e577bae374
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-13 16:21:25 -1000

    worked on smallest multiple, was unable to pass tests

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -8,7 +8,34 @@
  */
 module.exports = function( ceiling ) {
   // do work here
+  let smallestMultiple = 1;
+  let storedSmallestMultiple;
+  let foundSmallestMultiple = false;
+  let primeNumbers = {};
 
+  let divideLoop = function(divisor, total){
 
-  return 0;
+  };
+
+  for(let i = 1; i <= ceiling; i++){
+    //gets largest number to loop to
+    smallestMultiple *= i;
+  }
+  //loops through all numbers to highest number
+  for(let j = 1; j <= smallestMultiple; j++){
+    //loops through numbers to divide
+    for(let k = 1; k <= ceiling; k++){
+      //if mod returns 0 then change found to true, set storedSmallestMultiple to that
+      if(j % k === 0){
+        foundSmallestMultiple = true;
+        storedSmallestMultiple = j;
+      } else if (j % k !== 0){
+        foundSmallestMultiple = false;
+      }
+      if(foundSmallestMultiple === false){
+        break;
+      }
+    }
+  }
+  return storedSmallestMultiple;
 };
